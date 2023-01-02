df26326ca049825c2d719aae289f70d4e91da40a
aftab <sk.aftab1999@gmail.com>
2023-01-02 12:43:00 +0530

    Indent

--- src/components/QueueView.js
+++ src/components/QueueView.js
@@ -43,9 +43,15 @@ class QueueView extends Component {
             <div className={"buttons-container"}>
                 <div className={"add-node"}>
                     <input type="text" placeholder="Enter element" onChange={this.handleChange.bind(this)}/>
-                    <Button className={"add-node-button"} variant="primary" onClick={this.addToQueue.bind(this)} type="submit">Enqueue</Button>
+                    <Button className={"add-node-button"}
+                            variant="primary"
+                            onClick={this.addToQueue.bind(this)}
+                            type="submit">Enqueue</Button>
                 </div>
-                <Button className={"remove-node-button"} variant="primary" onClick={this.removeFromQueue.bind(this)} type="submit">Dequeue</Button>
+                <Button className={"remove-node-button"}
+                        variant="primary"
+                        onClick={this.removeFromQueue.bind(this)}
+                        type="submit">Dequeue</Button>
             </div>
         </div>
     }
